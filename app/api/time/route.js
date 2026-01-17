4f11632d16db6e114347c935bc6b48a4742d7882
manojsharma248 <35368092+manojsharma248@users.noreply.github.com>
2026-01-17 13:01:00 +0530

    feat(api): set revalidation time for time API endpoint

--- app/api/time/route.js
+++ app/api/time/route.js
@@ -1,4 +1,5 @@
 export const dynamic = "force-static";
+export const revalidate = 10; // seconds
 export async function GET() {
   return Response.json({ time: new Date().toLocaleTimeString() });
 }
